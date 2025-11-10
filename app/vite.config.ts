12421bcc7df78c30c933f62164c4a65e8ac74927
yangchanghun <gory4848@naver.com>
2025-11-10 17:23:11 +0900

    D

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
+
 export default defineConfig({
   plugins: [
     react(),
@@ -20,4 +22,11 @@ export default defineConfig({
       },
     }),
   ],
+
+  // ✅ 여기 추가!
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
