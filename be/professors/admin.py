5559f02dae02c52bd608b04244c74b582d22a9dc
yangchanghun <gory4848@naver.com>
2025-11-28 10:45:51 +0900

    D

--- be/professors/admin.py
+++ be/professors/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Visitors
+from .models import Professors
 
-admin.site.register(Visitors)
+admin.site.register(Professors)
